d1af3eb90087c00f7b13b0f82b554b01e26a12e6
QNguyen <quyen.nguyentrong.tpv@one-line.com>
2025-08-31 00:16:46 +0700

    sync-time-ws

--- StandardDate.js
+++ StandardDate.js
@@ -46,3 +46,87 @@ class StandardDate {
     return standardTimeRes;
   }
 }
+
+class StandardDateNew {
+  msDiff = 0;
+  createdAt = Date.now();
+  standardTimestamp = Date.now();
+  estimatedResponseTime = 80; // assume time from server response to client
+  localGreaterThanStandardInMs = 0;
+  timestampUrl;
+  greaterTimeUrl;
+  isUseLocal;
+
+  constructor({ timestampUrl, greaterTimeUrl, isUseLocal }) {
+    this.timestampUrl = timestampUrl;
+    this.greaterTimeUrl = greaterTimeUrl;
+    this.isUseLocal = isUseLocal;
+    this.init();
+  }
+
+  async init() {
+    if (this.isUseLocal) {
+      console.log(`use local timestamp as standard`);
+      return;
+    }
+
+    await this.syncTime();
+  }
+
+  //  Get server time at any specific time on local
+  getCurrentTime() {
+    // return this.standardTimestamp + Date.now() - this.createdAt;
+    return Date.now() - this.localGreaterThanStandardInMs;
+  }
+
+  async getStandardTimestamp() {
+    if (this.timestampUrl) {
+      // const timezone = "Etc/UTC";
+      const res = await fetch(this.timestampUrl);
+      const resData = await res.json();
+      return resData.data.timeStamp;
+    }
+
+    return Date.now();
+  }
+
+  async syncTime() {
+    // const resTimestamp = await this.getStandardTimestamp();
+    const beforeRequestTime = Date.now();
+    const greaterThanServer = await this.getGreaterThanServerTimestamp();
+    const sendTime =
+      Date.now() - beforeRequestTime - this.estimatedResponseTime;
+    this.localGreaterThanStandardInMs = greaterThanServer
+      ? greaterThanServer + sendTime
+      : 0;
+
+    document.getElementById(`test`).innerText = `
+                    local greater: ${
+                      Date.now() - this.localGreaterThanStandardInMs
+                    }
+                    greater than sv: ${greaterThanServer}
+                  `;
+
+    document.getElementById("time-greater-than-server").innerText =
+      this.localGreaterThanStandardInMs;
+    document.getElementById("request-time").innerText =
+      Date.now() - beforeRequestTime;
+  }
+
+  async getGreaterThanServerTimestamp() {
+    try {
+      if (this.greaterTimeUrl) {
+        // const timezone = "Etc/UTC";
+        const res = await fetch(
+          `${this.greaterTimeUrl}?localTimestamp=${Date.now()}`
+        );
+        const resData = await res.json();
+        return resData.data.localGreaterInMs;
+      }
+
+      return 0;
+    } catch (err) {
+      return 0;
+    }
+  }
+}
